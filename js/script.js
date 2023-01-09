0139badc556d2c5699f3995685bc42797e30e341
EvaXausa <xausa.eva@gmail.com>
2023-01-09 17:40:23 +0100

    change2

--- js/script.js
+++ js/script.js
@@ -1,6 +1,10 @@
 //stama un messaggio all'interno di un h1 con i data
 
 console.log("Vue ok", Vue);
-const app = Vue.createApp({});
+const app = Vue.createApp({
+  data() {
+    return { message: "Hello vue" };
+  },
+});
 
 app.amount("#root");
